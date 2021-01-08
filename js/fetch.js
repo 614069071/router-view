432edb5a6eb262e5f31d783d87de2bdcd23237a3
DESKTOP-5VAUTVG\JinTong <614069071>
2021-01-08 09:13:47 +0800

    u

--- js/fetch.js
+++ js/fetch.js
@@ -1,20 +1,20 @@
 // 登录
 function _login(parmas, callback) {
-    _request(parmas)
-        .then(function(res) {
-            if (res.error == 0) {
-                $.cookie('LoginStatus', true);
-                window.location.href = "/main.html";
-            } else if (res.error == 10001) {
-                _toast('密码错误');
-            } else {
-                _toast('登录失败');
-            }
-            callback && callback();
-        })
-        .catch(function(err) {
-            console.log(err.status);
-        });
+  _request(parmas)
+    .then(function (res) {
+      if (res.error == 0) {
+        $.cookie('LoginStatus', true);
+        window.location.href = "/main.html";
+      } else if (res.error == 10001) {
+        _toast('密码错误');
+      } else {
+        _toast('登录失败');
+      }
+      callback && callback();
+    })
+    .catch(function (err) {
+      console.log(err.status);
+    });
 }
 
 // 设置向导
@@ -24,162 +24,162 @@ function _setStaticConfigWizard(parmas) {
 
 // 获取日志
 function _getSystemLog(el, callback) {
-    var parmas = { operation: 'sys_log', function: 'get' };
-    var url = "http://" + document.domain + "/cgi-bin/log.sh";
+  var parmas = { operation: 'sys_log', function: 'get' };
+  var url = "http://" + document.domain + "/cgi-bin/log.sh";
 
-    _request(parmas, 'post', url)
-        .thne(function(res) {
-            var arr = res.data;
-            var list = '';
-            $.each(arr, function(i, e) {
-                list += '<tr><td>' + e.index + '</td><td>' + e.type + '</td><td>' + e.info + '</td></tr>';
-            })
-            el.empty().html(list);
-            callback && callback();
-        })
-        .catch(function(err) {
-            console.log(err);
-        })
+  _request(parmas, 'post', url)
+    .thne(function (res) {
+      var arr = res.data;
+      var list = '';
+      $.each(arr, function (i, e) {
+        list += '<tr><td>' + e.index + '</td><td>' + e.type + '</td><td>' + e.info + '</td></tr>';
+      })
+      el.empty().html(list);
+      callback && callback();
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
 }
 
 // 修改管理密码
 function updateManagePassword(oldPaswword, newPassword, confirmPassword, callback) {
-    var oldPaswword = _trim(oldPaswword);
-    var newPassword = _trim(newPassword);
-    var confirmPassword = _trim(confirmPassword);
+  var oldPaswword = _trim(oldPaswword);
+  var newPassword = _trim(newPassword);
+  var confirmPassword = _trim(confirmPassword);
 
-    if (!oldPaswword) {
-        _toast('请输入旧密码');
+  if (!oldPaswword) {
+    _toast('请输入旧密码');
+    return;
+  }
+
+  if (!newPassword) {
+    _toast('请输入新密码');
+    return;
+  }
+
+  var reg = /[\':;*?~`!@#$%^&+={}\[\]\<\\(\),\.\。\，]/;
+  if (newPassword.indexOf("%u") != -1 || reg.test(newPassword)) {
+    _toast('密码不能包含中文字符或者特殊字符！');
+    return;
+  }
+
+  if (newPassword.length < 5 || newPassword.length > 15) {
+    _toast('请输入5-15位新密码');
+    return;
+  }
+
+  if (!confirmPassword) {
+    _toast('请再次输入确认密码');
+    return;
+  }
+
+  if (newPassword != confirmPassword) {
+    _toast('两次密码不一致');
+    return;
+  }
+
+  var name = 'root';
+  var str_md5 = $.md5(name + oldPaswword);
+  var parmas = { operation: 'login', function: 'set', usrid: str_md5 };
+
+  // 验证旧密码
+  _request(parmas)
+    .then(function (res) {
+      if (res.error) {
+        _toast('旧密码验证错误');
         return;
-    }
+      }
 
-    if (!newPassword) {
-        _toast('请输入新密码');
-        return;
-    }
-
-    var reg = /[\':;*?~`!@#$%^&+={}\[\]\<\\(\),\.\。\，]/;
-    if (newPassword.indexOf("%u") != -1 || reg.test(newPassword)) {
-        _toast('密码不能包含中文字符或者特殊字符！');
-        return;
-    }
-
-    if (newPassword.length < 5 || newPassword.length > 15) {
-        _toast('请输入5-15位新密码');
-        return;
-    }
-
-    if (!confirmPassword) {
-        _toast('请再次输入确认密码');
-        return;
-    }
-
-    if (newPassword != confirmPassword) {
-        _toast('两次密码不一致');
-        return;
-    }
-
-    var name = 'root';
-    var str_md5 = $.md5(name + oldPaswword);
-    var parmas = { operation: 'login', function: 'set', usrid: str_md5 };
-
-    // 验证旧密码
-    _request(parmas)
-        .then(function(res) {
-            if (res.error) {
-                _toast('旧密码验证错误');
-                return;
-            }
-
-            setAccount(newPassword, callback);
-        })
-        .catch(function(err) {
-            console.log(err, 'updateManagePassword')
-        })
+      setAccount(newPassword, callback);
+    })
+    .catch(function (err) {
+      console.log(err, 'updateManagePassword')
+    })
 }
 
 // 修改管理密码
 function setAccount(password, callback) {
-    var modfiy_parmas = { operation: 'login_modfiy', user: 'admin', password: password };
-    _request(modfiy_parmas)
-        .then(function(res) {
-            // 修改成功
-            callback && callback(res);
-        })
-        .catch(function(err) {
-            console.log(err, 'setAccount')
-        })
+  var modfiy_parmas = { operation: 'login_modfiy', user: 'admin', password: password };
+  _request(modfiy_parmas)
+    .then(function (res) {
+      // 修改成功
+      callback && callback(res);
+    })
+    .catch(function (err) {
+      console.log(err, 'setAccount')
+    })
 }
 
 // 固件升级
 function uploadFile(file) {
-    var formData = new FormData();
-    formData.append('file', file);
-    formData.append('type', 'upload');
-    formData.append('function', 'upgrade');
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('type', 'upload');
+  formData.append('function', 'upgrade');
 
-    _request(formData)
-        .then(function(res) {
-            console.log(res)
-            getUpdateFileStatus();
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
+  _request(formData)
+    .then(function (res) {
+      console.log(res)
+      // getUpdateFileStatus();
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
 }
 
 // 获取升级返回状态
 function getUpdateFileStatus() {
-    var parmas = { type: 'mtd_write', function: 'upgrade' };
+  var parmas = { type: 'mtd_write', function: 'upgrade' };
 
-    _request(parmas)
-        .then(function(res) {
-            console.log(res);
-            if (res.error == 0) {
-                if (res.status == 1) {
-                    clearInterval(interval);
-                    isGet = true;
-                    timeout = 0;
+  _request(parmas)
+    .then(function (res) {
+      console.log(res);
+      if (res.error == 0) {
+        if (res.status == 1) {
+          clearInterval(interval);
+          isGet = true;
+          timeout = 0;
 
-                    if (res.check == 1) {
-                        // 升级中...
-                        //正在升级，大约需要2分钟。升级过程中请勿操作或断电...
-                        // var content = Upgrading_tips1;
-                        // GetProgressBar(content);//进度条
-                    } else {
-                        _toast('升级失败');
-                    }
+          if (res.check == 1) {
+            // 升级中...
+            //正在升级，大约需要2分钟。升级过程中请勿操作或断电...
+            // var content = Upgrading_tips1;
+            // GetProgressBar(content);//进度条
+          } else {
+            _toast('升级失败');
+          }
 
-                }
-            } else {
-                console.log(res.error)
-            }
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
+        }
+      } else {
+        console.log(res.error)
+      }
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
 }
 
 // 重启路由器
 function restartRoute() {
-    var params = { operation: 'device_opt', action: 'reboot' };
-    _request(params)
-        .then(function(res) {
-            console.log(res)
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
+  var params = { operation: 'device_opt', action: 'reboot' };
+  _request(params)
+    .then(function (res) {
+      console.log(res)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
 }
 
 // 恢复出厂设置
 function restoreRoute() {
-    var params = { operation: 'device_opt', action: 'default' };
-    _request(params)
-        .then(function(res) {
-            console.log(res)
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
+  var params = { operation: 'device_opt', action: 'default' };
+  _request(params)
+    .then(function (res) {
+      console.log(res)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
 }
--- js/index.js
+++ js/index.js
@@ -9,31 +9,31 @@
 // }
 
 var menuBarArray = [
-    // {
-    //   title: '网络状态',
-    //   module: 'network-status',
-    //   checked: 1
-    // },
-    { title: '设置向导', module: 'route-guide', checked: 1 },
-    {
-        title: '路由设置',
-        list: [
-            { title: '路由器状态', module: 'route-status' },
-            { title: 'DHCP服务器', module: 'device-manage' },
-            { title: '上网设置', module: 'route-internet' },
-            { title: 'LAN口设置', module: 'route-lan' },
-            { title: '路由器管理', module: 'route-manage' },
-            { title: '系统日志', module: 'route-log' },
-        ]
-    },
-    {
-        title: '云盘操作',
-        list: [
-            { title: '用户信息', module: 'cloud-userinfo' },
-            { title: '磁盘操作', module: 'cloud-control' },
-            { title: '应用下载', module: 'cloud-download' },
-        ]
-    },
+  // {
+  //   title: '网络状态',
+  //   module: 'network-status',
+  //   checked: 1
+  // },
+  { title: '设置向导', module: 'route-guide', checked: 1 },
+  {
+    title: '路由设置',
+    list: [
+      { title: '路由器状态', module: 'route-status' },
+      { title: 'DHCP服务器', module: 'device-manage' },
+      { title: '上网设置', module: 'route-internet' },
+      { title: 'LAN口设置', module: 'route-lan' },
+      { title: '路由器管理', module: 'route-manage' },
+      { title: '系统日志', module: 'route-log' },
+    ]
+  },
+  {
+    title: '云盘操作',
+    list: [
+      { title: '用户信息', module: 'cloud-userinfo' },
+      { title: '磁盘操作', module: 'cloud-control' },
+      { title: '应用下载', module: 'cloud-download' },
+    ]
+  },
 ];
 
 // 创建 NavMenu
@@ -41,18 +41,18 @@ new _MenuTree(menuBarArray, $('.nav-menu-wrapper'));
 
 // DCHP设备列表创建分页器
 new _Pager($('.device-list-pager-wrapper'), {
-    total: 100,
-    callback: function(index) {
-        console.log('device', index);
-    }
+  total: 100,
+  callback: function (index) {
+    console.log('device', index);
+  }
 });
 
 // 系统日志 分页器
 new _Pager($('.roter-log-pager-wrapper'), {
-    total: 100,
-    callback: function(index) {
-        console.log('log', index);
-    }
+  total: 100,
+  callback: function (index) {
+    console.log('log', index);
+  }
 });
 
 /* 主体功能模块 */
@@ -61,27 +61,27 @@ var $connectRadioBtns = $('.connect-type-wrapper .check-type');
 var $connectModels = $('.connect-type-main-wrapper .model-type-item');
 
 // tab切换
-$connectRadioBtns.on('change', function() {
-    var $index = $(this).data('index');
-    $connectModels.hide().eq($index).show();
+$connectRadioBtns.on('change', function () {
+  var $index = $(this).data('index');
+  $connectModels.hide().eq($index).show();
 });
 
 // lan设置
 var $lanRadios = $('#setting_lan_form .check-type');
 var $lanTypeItems = $('#setting_lan_form .lan-type-item');
 
-$lanRadios.on('change', function() {
-    var $index = $(this).data('index');
-    $lanTypeItems.hide().eq($index).show();
+$lanRadios.on('change', function () {
+  var $index = $(this).data('index');
+  $lanTypeItems.hide().eq($index).show();
 });
 
 // 设备管理 => 修改管理密码
 var $setting_manage_form = $('#setting_manage_form');
 var $setting_manage_form_submit = $('#setting_manage_form_submit');
 
-$setting_manage_form_submit.on('click', function() {
-    var data = _formArrToObject($setting_manage_form);
-    updateManagePassword(data.oldPassword, data.newPassword, data.confirmPassword);
+$setting_manage_form_submit.on('click', function () {
+  var data = _formArrToObject($setting_manage_form);
+  updateManagePassword(data.oldPassword, data.newPassword, data.confirmPassword);
 });
 
 // 设备管理 => 固件升级
@@ -91,44 +91,44 @@ var $progressBarwrapper = $('.progress-bar-wrapper');
 var $upgrade_name = $('.upgrade-name');
 // var upgrade_Progress = null;
 
-$update_file.on('change', function(e) {
-    var file = e.target.files[0] || {};
-    var name = file.name;
-    name && $upgrade_name.text(name);
+$update_file.on('change', function (e) {
+  var file = e.target.files[0] || {};
+  var name = file.name;
+  name && $upgrade_name.text(name);
 })
 
-var upgrade_Progress = new _Progress($progressBarwrapper)
+var upgrade_Progress = new _Progress($progressBarwrapper, 120)
 
-$upgrade_submit.on('click', function() {
-    var $file = $update_file.prop('files')[0];
-    if (!$file) {
-        _toast('请选择升级文件！');
-        return;
-    }
-    upgrade_Progress.start();
-    uploadFile($file);
+$upgrade_submit.on('click', function () {
+  var $file = $update_file.prop('files')[0];
+  if (!$file) {
+    _toast('请选择升级文件！');
+    return;
+  }
+  upgrade_Progress.start();
+  uploadFile($file);
 })
 
 // 设备管理 => 重启
 var $restartDeviceBtn = $('#restart_device_btn');
 
-$restartDeviceBtn.on('click', function() {
-    _dialog({
-        success: function() {
-            restartRoute();
-            // _close();
-        }
-    });
+$restartDeviceBtn.on('click', function () {
+  _dialog({
+    success: function () {
+      restartRoute();
+      // _close();
+    }
+  });
 });
 
 // 设备管理 => 恢复出厂设置
 var $restoreDeviceBtn = $('#restore_device_btn');
 
-$restoreDeviceBtn.on('click', function() {
-    _dialog({
-        success: function() {
-            // restoreRoute();
-            // _close();
-        }
-    });
+$restoreDeviceBtn.on('click', function () {
+  _dialog({
+    success: function () {
+      // restoreRoute();
+      // _close();
+    }
+  });
 });
