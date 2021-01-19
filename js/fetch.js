0802ace9a811f79c20a1022df44ab77b117e4d8f
DESKTOP-5VAUTVG\JinTong <614069071>
2021-01-19 20:33:50 +0800

    优化样式及添加接口

--- js/fetch.js
+++ js/fetch.js
@@ -6,37 +6,11 @@ function _login(password) {
   return _request(parmas);
 }
 
-// 获取日志
-function _getSystemLog() {
-  var parmas = { operation: 'sys_log', function: 'get' };
-  var url = "http://" + document.domain + "/cgi-bin/log.sh";
-
-  return _request(parmas, 'post', url);
-}
-
 // 修改管理密码
 function _setAccount(password) {
   var modfiy_parmas = { operation: 'login_modfiy', user: 'admin', password: password };
 
   return _request(modfiy_parmas)
-}
-
-// 固件升级
-function _uploadFile(file) {
-  var formData = new FormData();
-  formData.append('file', file);
-  formData.append('type', 'upload');
-  formData.append('function', 'upgrade');
-
-  return _request(formData)
-  // getUpdateFileStatus();// 获取升级状态
-}
-
-// 获取升级返回状态
-function _getUpdateFileStatus() {
-  var parmas = { type: 'mtd_write', function: 'upgrade' };
-
-  return _request(parmas)
 }
 
 // 重启路由器
@@ -53,10 +27,73 @@ function _restoreRoute() {
   return _request(params);
 }
 
+// 首页获取路由信息
+function _getRouterInfo() {
+  var parmas = { operation: 'route_info', function: 'get' };
+
+  return _request(parmas);
+}
+
+// 获取设备状态信息（）
+
+// 获取时间
+function _getTimeSync() {
+  var parmas = { operation: 'time_sync', function: 'get', server: '' };
+
+  return _request(parmas);
+}
+
+// lan口设置 获取DHCP信息
+function _getLanInfo() {
+  var parmas = { operation: 'dhcpd', function: 'get' };
+
+  return _request(parmas);
+}
+
+//  lan口设置 => 设置lan口信息
+function _setLanInfo() {
+  var parmas = {
+    operation: 'dhcpd',
+    function: 'set',
+    start: '',//开始区间
+    end: '',//结束区间
+    mask: '',//子网掩码
+    ip: ip,
+    enable: 1,//是否打开
+    dns1: '',
+    dns2: '',
+    ip_limit: '',
+    ip_startthird: '',
+  };
+
+  return _request(parmas);
+}
+
+// lan口设置 => 获取dhcp设备列表
+function _getOlineList() {
+  var parmas = { operation: 'get_host', function: 'get' };
+
+  return _request(parmas);
+}
+
+// wan口设置 设置dhcp
+function _setConnectDhcp(dns, mac, mtu) {
+  var parmas = {
+    operation: 'wan_setup',
+    function: 'set',
+    mode: 1,
+    dns: dns,
+    mac: mac,
+    mtu: mtu
+  };
+
+  return _request(parmas);
+}
+
 // 宽带拨号
 function _setConnectPPPoE(account, password) {
   var params = {
-    operation: "wan_setup_wizard",
+    operation: "wan_setup",
     function: "set",
     user: account,//待确认
     passwd: password,//待确认
@@ -70,27 +107,6 @@ function _setConnectPPPoE(account, password) {
   };
 
   return _request(params);
-}
-
-// 获取路由信息
-function _getRouterInfo() {
-  var parmas = { operation: 'route_info', function: 'get' };
-
-  return _request(parmas);
-}
-
-// 获取时间
-function _getTimeSync() {
-  var parmas = { operation: 'time_sync', function: 'get', server: '' };
-
-  return _request(parmas);
-}
-
-// 获取dhcp设备列表
-function _getOlineList() {
-  var parmas = { operation: 'get_host', function: 'get' };
-
-  return _request(parmas);
 }
 
 // 设置静态IP
@@ -107,6 +123,25 @@ function _setConnectStatic(ip, mask, gw, dns, dns1, mac, mtu) {
     dns1: dns1,
     mac: mac,
   };
+
+  return _request(parmas);
+}
+
+// 软件升级 => 上传文件
+function _uploadFile(file) {
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('type', 'upload');
+  formData.append('function', 'upgrade');
+
+  return _request(formData);
+  // 上传完成，开始升级
+  // _upgradeStart();
+}
+
+//  软件升级 => 升级
+function _upgradeStart() {
+  var parmas = { type: 'mtd_write', function: 'upgrade' };
 
   return _request(parmas);
 }
