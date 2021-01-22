f80cfd13923057f0b8923b883e5e6eba6c06110e
JinTong <tong.jin@cdatatec.com>
2021-01-22 17:17:24 +0800

    u

--- js/common.js
+++ js/common.js
@@ -1,6 +1,5 @@
 // 请求地址
-window._action = "http://" + document.domain + "/cgi-bin/cdata.cgi";
-window._guide = -1; // guide  = 0 when checkconfig(),else guide = 1 when click menu .a
+window._action = "/cgi-bin/cdata.cgi";
 
 // 禁止右键
 function disEvent() {
